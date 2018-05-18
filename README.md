d1b7b932ff099ab66c2bc990fdbed71300f37c3d
Paulina Dabrowska <14799673+Trufs@users.noreply.github.com>
2018-05-18 17:08:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Feed Reader Testing
+A Jasmine testing suite for a web-based application that reads RSS feeds.
+
+### Prerequisites
+The app can be run in any browser which supports ES6. Internet connection is required.
+
+### Installing
+No installation is needed. All you need to do is open the index.html file in your browser.
+
+### Instructions
+#### Basic app functionality
+Click on the hamburger icon to open the menu. You can choose a feed from the menu to load new entries. Once you found an entry that interests you, you can click on the link to go to its webpage.
+#### Testing Suite
+At the bottom of the page, you can see Jasmine tests and their results. Code for the tests is available in **./jasmine/spec/feedreader.js**.
+Tests performed by the suite:
+1. Tests if the feeds are defined, and do they contain a name and a url.
+2. Tests if the menu is hidden by default and changes visibility once the menu icon is clicked.
+3. Tests if there is at least one entry available after the initial loading of the app.
+4. Tests if the content changes after choosing a new feed.
+
+### Acknowledgments
+* The starter code for this project was provided by Udacity.
+* Testing done with [Jasmine](http://jasmine.github.io/)
+* Templating from [Handlebars](https://handlebarsjs.com/)
+* Application uses Google Feed Reader API.
+
+### Dependencies
+* [Jasmine](http://jasmine.github.io/)
+* [Handlebars](https://handlebarsjs.com/)
+* Google Feed Reader API
+* Google Roboto font
+
+### Contributing
+This is a project for Udacity course and it will likely not be developed further.
+
+### License
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/Trufs/arcade-game-project/blob/master/LICENSE) file for details.
+
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,100 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+
+$(function() {
+
+    // A suite testing RSS feeds definitions.
+    describe('RSS Feeds', function(){
+
+        /* Check if allFeeds variable has been defined and that it is not
+         * empty.
+         */
+        it('are defined', function(){
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        /* Check if each feed in the allFeeds object has a URL defined
+         * and that the URL is not empty.
+         */
+        it('have a URL defined', function(){
+                allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                });
+        });
+
+        /* Check if each feed in the allFeeds object has a name defined
+         * and that the name is not empty.
+         */
+        it('have a name defined', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+            });
+        });
+    });
+
+
+    // A suite testing menu visibility.
+    describe('The menu', function(){
+
+        // Check if the menu element is hidden by default.
+        it('is hidden by default', function(){
+            expect($('body').attr('class')).toBe('menu-hidden');
+        });
+
+         // Check if the menu changes visibility when the menu icon is clicked.
+        it('changes visibility when the icon is clicked', function(){
+            const event = {
+                    type: 'click',
+                }
+            $('.menu-icon-link').trigger(event); //trigger an event happening when user clicks the menu icon
+            expect($('body').attr('class')).toBe('');
+            $('.menu-icon-link').trigger(event);
+            expect($('body').attr('class')).toBe('menu-hidden');
+        });
+    });
+
+
+    //A suite testing initial feed loading.
+    describe('Initial Entries', function(done){
+        const feed = $('.feed');
+        beforeEach(function(done){
+                loadFeed(0, done);
+        });
+
+        /* Check if when the loadFeed function is called and completes its work, there is at least
+         * one .entry element within the .feed container.
+         */
+        it('there is at least one entry within the feed container', function(){
+            expect(feed.children().toArray().length).not.toBe(0);
+        });
+    });
+
+    //A suite testing loading a different feed.
+    describe('New Feed Selection', function(done){
+        let previousContent;
+        const id = 1;
+
+        beforeEach(function(done){
+            previousContent = $('.feed').html();
+            loadFeed(id, done);
+        });
+
+        //Check if the content changes when a new feed is loaded.
+        it('when a new feed is loaded, the content changes', function(){
+            const event = {
+                    type: 'click',
+                }
+            $('.feed-list').trigger(event); //trigger an event happening when user clicks a link in the menu
+            expect($('.feed').html()).not.toEqual(previousContent);
+        });
+    });
+}());
